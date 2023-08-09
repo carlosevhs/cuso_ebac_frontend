6a1360b546d944ffae215c888b84d445513d4d32
Eduardo Vieira <carlosevhs@gmail.com>
2023-08-09 17:35:27 -0300

    commit inicial

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaNumeros(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicaNumeros(5, 20));
+console.log(saudacao('Maria'));
---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+function multiplicaNumeros(num1: number , num2: number): number {
+    return num1*num2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`
+}
+
+
+console.log(multiplicaNumeros (5,20));
+console.log(saudacao ('Maria'));
+
