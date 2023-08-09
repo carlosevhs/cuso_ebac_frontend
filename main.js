35dfa4e650604b48f43e20f5fa5eb406906f508a
Eduardo Vieira <carlosevhs@gmail.com>
2023-08-09 09:27:45 -0300

    criando classes herdeiras

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-function Celular(sistemaOperacional, modelo, memoria) {
+function Celular (sistemaOperacional, marca) {
     this.sistemaOperacional = sistemaOperacional;
-    this.modelo = modelo;
-    this.memoria = memoria;
-    this.fotografar = function(){
-        console.log("click");
-    }
+    this.marca = marca;
     
 }
 
+function Smartphone (sistemaOperacional, marca, modelo, memoria ) {
+    this.modelo = modelo;
+    this.memoria = memoria;
 
-const celularDoJoao = new Celular("IOS","Iphone 14 PRO MAX", "512 Gb");
-const celularDaMaria = new Celular("Android","Galaxy S22 Ultra", "256 Gb");
-const celularDoPedro = new Celular("Android","Xiaomi Mi 13 Ultra", "1 Tb");
+    Celular.call(this, sistemaOperacional,marca);
+    
+}
+
+function TelefoneFixo (sistemaOperacional, marca, modelo, tipo){
+    this.modelo = modelo;
+    this.tipo = tipo
+
+    Celular.call(this, sistemaOperacional,marca);
+}
 
 
-console.log(celularDoJoao);
-console.log(celularDaMaria);
-console.log(celularDoPedro);
+const celularDoJoao = new Smartphone("IOS","Apple","Iphone 14 PRO MAX", "512 Gb");
+const celularDaMaria = new Smartphone("Android","Samsung","Galaxy S22 Ultra", "256 Gb");
+const celularDoPedro = new TelefoneFixo("Sismens","IntelBrass","TS3111", "Sem fio");
+
+
+console.log(`O celular do João é um `, celularDoJoao);
+console.log(`O celular da Maria é um `,celularDaMaria);
+console.log(`O celular do Pedro é um `,celularDoPedro);
